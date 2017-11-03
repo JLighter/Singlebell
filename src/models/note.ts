f935820d06c6f9e97255f2045473f1e64d35510e
Heroguelle Julien <heroguelle.julien@gmail.com>
2017-11-03 12:13:48 +0100

    Update note model and assets filename

--- src/models/note.ts
+++ src/models/note.ts
@@ -2,12 +2,10 @@ export class Note {
 
   constructor(
     public name: String,
-    public fileName: String,
     public postion : Number
   )
   {
     this.name = name;
-    this.fileName = fileName;
     this.postion = postion;
   }
 }
