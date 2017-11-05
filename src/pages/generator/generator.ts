3ccb49f8cc725ad3de6dc5afb39d5cffdc049590
Heroguelle Julien <heroguelle.julien@gmail.com>
2017-11-05 14:47:27 +0100

    Adding tone.js and extra for testing

--- src/pages/generator/generator.ts
+++ src/pages/generator/generator.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ExerciceGenerator} from "../../utilities/exercice_generator";
 import {Exercice} from "../../models/exercice";
-
+import Tone from 'tone';
 /**
  * Generated class for the GeneratorPage page.
  *
@@ -19,6 +19,8 @@ import {Exercice} from "../../models/exercice";
 export class GeneratorPage {
 
   private exercice: Exercice;
+
+  private notes: Array<Note>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public generator: ExerciceGenerator) {
 
@@ -39,7 +41,37 @@ export class GeneratorPage {
     _this.generator.newQuestion(_this.exercice).then(function(question) {
       _this.exercice.questions.push(question);
       console.log(question);
+      _this.notes = question.notes;
     }, (error) => console.error(error))
+  }
+
+  generateSound(tone, height) {
+    let _this = this;
+    let synth = new Tone.Synth().toMaster();
+
+
+    if (tone) {
+      synth.triggerAttackRelease(tone+height, "8n")
+    } else {
+      let timeout = 500;
+
+      setTimeout(()=>{
+        synth.triggerAttackRelease(this.notes[0].name, "8n")
+
+        setTimeout(()=>{
+          synth.triggerAttackRelease(this.notes[1].name, "8n")
+
+          setTimeout(()=>{
+            synth.triggerAttackRelease(this.notes[2].name, "8n")
+          }, timeout)
+
+        }, timeout)
+
+      }, timeout);
+
+    }
+
+
   }
 
 }
